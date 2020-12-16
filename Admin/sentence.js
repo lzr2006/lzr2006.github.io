0c0b2c3c71888d3dc91f0e007bcefc67013ff4e7
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-12-17 06:51:48 +0800

    Create sentence.js

--- Admin/sentence.js
+++ Admin/sentence.js
@@ -9,3 +9,34 @@ $(function()
     console.log(data)
     $(".sentence").text(data)
   })
+  //点赞功能
+  $("#like").click(function()
+  {
+    if(likes<1)
+    {
+    likes+=1
+    console.log('喜欢->'+likes)
+    $("#like").css('background-color',"red")
+    alert("喜欢->"+likes)
+    }
+    else
+    {
+      alert("你已经点过赞了，不要重复刷赞")
+    }
+  })
+  //取消点赞
+  $("#unlike").click(function()
+  {
+    likes=0
+    console.log("喜欢->"+likes)
+    alert("已取消喜欢")
+    $("#like").css("background-color","white")
+  })
+  //上传数据到数据库
+  function upload_likes_to_mysql()
+  {
+    $.post("upload_likes.php",{},function(data,status)
+    {
+
+    })
+  }
